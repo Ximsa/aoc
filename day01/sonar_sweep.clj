56e93d367a579eb40a14709bbe25b943ca5f2c05
Ximsa <burrfelix@aol.de>
2021-12-02 23:30:49 +0100

    day1

---
+++ day01/sonar_sweep.clj
@@ -0,0 +1,17 @@
+(use '[clojure.string])
+
+(defn parse [file_name]
+  (map #(Integer/parseInt %1) (split-lines(slurp file_name))))
+
+(defn sonar_sweep [sonar_data]
+  (reduce (fn [acc strength]
+            {:sum (+ (:sum acc) (if (> strength (:last acc)) 1 0))
+             :last strength})
+          {:sum 0 :last Integer/MAX_VALUE}
+          sonar_data))
+
+(defn sonar_sweep_1 [file_name]
+  (sonar_sweep (parse file_name)))
+
+(defn sonar_sweep_2 [file_name]
+  (sonar_sweep (map (partial apply +) (partition 3 1 (parse file_name)))))

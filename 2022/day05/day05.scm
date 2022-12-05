e41c6cd1e3d2b39af4a910520846662a678a7587
Felix Burr <felix@DESKTOP-V8KO5C9.localdomain>
2022-12-05 14:57:46 +0100

    refactor

--- 2022/day05/day05.scm
+++ 2022/day05/day05.scm
@@ -6,12 +6,11 @@
 (define (inc x) (+ x 1))
 (define (dec x) (- x 1))
 (define (id x) x)
-
 (define (reduce f y xs)
+  "fold left"
   (if (null? xs)
       y
       (reduce f (f y (car xs)) (cdr xs))))
-
 
 (define (nth list n)
   "return the n-th element of a list"
@@ -38,15 +37,13 @@
       (cons (car list) (replace-nth (cdr list) (dec n) item))))
 
 ;; actual aoc specific 
-
 (define (parse file-name)
   "returns crane commands"
   (let* ((contents (call-with-input-file file-name get-string-all))
 	 (lines (map match:substring (list-matches "[^\n]+" contents)))
 	 (commands (map (lambda (line) ;; for each line
-			  (map dec ;; function composition would be nice
-			       (map string->number
-				    (map match:substring (list-matches "[0-9]+" line))))) ;; extract numbers
+			  (map (lambda (numbers) (dec (string->number (match:substring numbers))))
+			       (list-matches "[0-9]+" line))) ;; extract numbers
 			lines)))
     commands))
 
@@ -56,10 +53,8 @@
 					 "ZLSFQRRPD" "WPFDHLSC" "ZGNFPMSD")))
 
 (define (move crates n src target)
-  (let* ((operation reverse) ;; replace with reverse for part 1
-	 (n (inc n))
-	 (src-stack (nth crates src))
-	 (elems (operation (take src-stack n)))
+  (let* ((src-stack (nth crates src))
+	 (elems (id (take src-stack n))) ;; replace id with reverse for part 1
 	 (src-stack (drop src-stack n))
 	 (target-stack (append elems (nth crates target))))
     (replace-nth (replace-nth crates src src-stack) ;; replace both new stacks
@@ -68,10 +63,9 @@
 (list->string
  (map car
       (reduce (lambda (crates command)
-		(let* ((n (nth command 0))
+		(let* ((n (inc (nth command 0)))
 		       (src (nth command 1))
 		       (target (nth command 2)))
 		  (move crates n src target)))
 	      input-crates
 	      (parse "input"))))
-

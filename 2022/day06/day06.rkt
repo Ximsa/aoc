8e70364413381ee026496246497e9aed7756f66a
Felix Burr <felix@welcome-dynamic-clients.wlan.uni-ulm.de>
2022-12-06 14:46:24 +0100

    readded day6

--- 2022/day06/day06.rkt
+++ 2022/day06/day06.rkt
@@ -1,1 +1,22 @@
-(define contents (port->string (open-input-file "input") #:close? #t))
+(require racket/set)
+
+(define input (string->list (port->string (open-input-file "input") #:close? #t)))
+(define sample (string->list "mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+
+(define (partition n step coll)
+  "partitions a list into n large sublists with step"
+  (if (> n (length coll))
+      '()
+      (cons (take coll n) (partition n step (drop coll step)))))
+  
+
+(define (solve contents size)
+  (car (filter number?
+	       (for/list ([n (build-list (length contents) (lambda (x) (+ x size)))]
+			  [xs (partition size 1 contents)])
+		 (if (= size (length (set->list (apply set xs))))
+		     n
+		     false)))))
+
+(solve input 4) ;; part 1
+(solve input 14) ;; part 2

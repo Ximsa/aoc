148904a3e3cf1a90809bf7c187561535addd212c
Felix Burr <felix@DESKTOP-V8KO5C9.localdomain>
2022-12-13 15:55:03 +0100

    cleanup

--- 2022/day13/day13.clj
+++ 2022/day13/day13.clj
@@ -11,16 +11,9 @@
                     (if (zero? result)
                       (decide-order (rest xs) (rest ys))
                       result))
-    [true false] 1 ;; right order if xs empty first
-    [false true] -1 ;; wrong order if ys empty first
+    [true false] -1 ;; right order if xs empty first
+    [false true] 1 ;; wrong order if ys empty first
     [true true] 0)) ;; equal
-
-(defn compare-integers [x y] (compare y x))
-
-(defn compare-integer-list [x y]
-  (case [(number? x) (number? y)]
-    [true false] (decide-order [x] y)
-    [false true] (decide-order x [y])))
 
 (defn decide-order
   ([[xs ys]] (decide-order xs ys))
@@ -28,12 +21,12 @@
              [false false] (compare-lists xs ys)
              [true false] (decide-order [xs] ys)
              [false true] (decide-order xs [ys])
-             [true true] (compare-integers xs ys))))
+             [true true] (compare xs ys))))
 ;;part 1
 (apply + (map-indexed
-          (fn [index value] (if (> (decide-order value) 0) (inc index) 0))
+          (fn [index value] (if (< (decide-order value) 0) (inc index) 0))
           (partition 2 input)))
 ;; part 2
-(let [sorted-signals (reverse (sort decide-order (conj input [[6]] [[2]])))]
+(let [sorted-signals (sort decide-order (conj input [[6]] [[2]]))]
   (* (inc (.indexOf sorted-signals [[6]]))
      (inc (.indexOf sorted-signals [[2]]))))
